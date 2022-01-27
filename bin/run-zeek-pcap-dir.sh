e1b13beeec99ccf3925f8eeb6d755beebfa4abc5
Peter Reuterås <peter@reuteras.com>
2022-01-27 12:34:28 +0100

    Switch to container from Docker Hub.

--- bin/run-zeek-pcap-dir.sh
+++ bin/run-zeek-pcap-dir.sh
@@ -6,19 +6,19 @@ COUNT=$(find pcap -type f | wc -l | awk '{print $1}')
 [[ -e input/input.pcap ]] && rm -f input/input.pcap
 
 function run_container() {
-    docker run --rm -v "${PWD}"/input:/input -v "${PWD}"/pcap:/pcap:ro container-network-tools mergecap -w input/input.pcap pcap/*
+    docker run --rm -v "${PWD}"/input:/input -v "${PWD}"/pcap:/pcap:ro reuteras/container-alpine-network mergecap -w input/input.pcap pcap/*
 }
 
 if [[ ${COUNT} -gt 1 ]]; then
-    if docker images -a | grep -E ^container-network-tools > /dev/null 2>&1 ; then
+    if docker images -a | grep -E ^reuteras/container-alpine-network > /dev/null 2>&1 ; then
         run_container
     else
-        echo "No container-network-tools container. Build."
-        docker build --tag=container-network-tools tools
+        echo "No reuteras/container-alpine-network container. Pull."
+        docker pull reuteras/container-alpine-network
         run_container
     fi
 fi
 
-docker run -it --rm -v "${PWD}"/input:/pcap:ro -v "${PWD}"/output:/output -w /output container-zeek zeek -C -r "/pcap/input.pcap"
+docker run -it --rm -v "${PWD}"/input:/pcap:ro -v "${PWD}"/output:/output -w /output reuteras/container-zeek zeek -C -r "/pcap/input.pcap"
 
 rm -rf input
